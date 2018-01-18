ebad90d7d59fa35f9c4771d8a38b94016de05813
Stefano <you@example.com>
2018-01-18 12:03:37 +0100

    cha

--- waocuda.cu
+++ waocuda.cu
@@ -23,6 +23,7 @@
 #include <cstdio>
 #include <sys/mman.h>
 #include <cooperative_groups.h>
+#define PI_F 3.141592654f
 
 
 struct joints{
@@ -81,16 +82,12 @@ __device__ void fobj(joints currpos,float* tmpscore)
   *tmpscore=sqrt(currpos.jointsval[0]*currpos.jointsval[0]+currpos.jointsval[1]*currpos.jointsval[1]+currpos.jointsval[2]*currpos.jointsval[2]+currpos.jointsval[3]*currpos.jointsval[3]+currpos.jointsval[4]*currpos.jointsval[4]+currpos.jointsval[5]*currpos.jointsval[5]);
 }
 
-__global__ void WaoCycle(boundaries limit,int n_cycles,float* bestscore,joints* bestjoint,int n_joints)//rifare in funz of n_joints
+__global__ void WaoCycle(boundaries limit,int n_cycles,float* bestscore,joints* bestjoint)
 {
-  float rnd_sel;
-  joints jointval;
   joints Leader_pos;
   float tmpscore;
-  float a,a2,A,C,b,l,p,D_X_rand,X_rand;
-  bool chkl1,chkl2,chkl3,chkl4,chkl5,chkl6,chku1,chku2,chku3,chku4,chku5,chku6,chmin;
-  int* tmppnt;
-  int rand_leader_index[n_joints];
+  float a,a2,A,C,b,l,D_X_rand,X_rand;
+  bool chkl1,chkl2,chkl3,chkl4,chkl5,chkl6,chku1,chku2,chku3,chku4,chku5,chku6;
   
   extern __shared__ joints shmem[];
   joints * jointshar  = (joints *)&shmem;
@@ -98,44 +95,42 @@ __global__ void WaoCycle(boundaries limit,int n_cycles,float* bestscore,joints* 
   curandState_t state;
   curand_init(clock64() ,threadIdx.x, 0, &state);
   
-  jointval.jointsval[0]=curand_uniform(&state)*(limit.joint1b[1]-limit.joint1b[0])+limit.joint1b[0];
-  jointval.jointsval[1]=curand_uniform(&state)*(limit.joint2b[1]-limit.joint2b[0])+limit.joint2b[0];
-  jointval.jointsval[2]=curand_uniform(&state)*(limit.joint3b[1]-limit.joint3b[0])+limit.joint3b[0];
-  jointval.jointsval[3]=curand_uniform(&state)*(limit.joint4b[1]-limit.joint4b[0])+limit.joint4b[0];
-  jointval.jointsval[4]=curand_uniform(&state)*(limit.joint5b[1]-limit.joint5b[0])+limit.joint5b[0];
-  jointval.jointsval[5]=curand_uniform(&state)*(limit.joint6b[1]-limit.joint6b[0])+limit.joint6b[0];
-  
+  jointshar[threadIdx.x].jointsval[0]=curand_uniform(&state)*(limit.joint1b[1]-limit.joint1b[0])+limit.joint1b[0];
+  jointshar[threadIdx.x].jointsval[1]=curand_uniform(&state)*(limit.joint2b[1]-limit.joint2b[0])+limit.joint2b[0];
+  jointshar[threadIdx.x].jointsval[2]=curand_uniform(&state)*(limit.joint3b[1]-limit.joint3b[0])+limit.joint3b[0];
+  jointshar[threadIdx.x].jointsval[3]=curand_uniform(&state)*(limit.joint4b[1]-limit.joint4b[0])+limit.joint4b[0];
+  jointshar[threadIdx.x].jointsval[4]=curand_uniform(&state)*(limit.joint5b[1]-limit.joint5b[0])+limit.joint5b[0];
+  jointshar[threadIdx.x].jointsval[5]=curand_uniform(&state)*(limit.joint6b[1]-limit.joint6b[0])+limit.joint6b[0];
   for(int cyc=0;cyc<n_cycles;cyc++)
   {
-    chkl1 = (jointval.jointsval[0]<limit.joint1b[0]);
-    chkl2 = (jointval.jointsval[1]<limit.joint2b[0]);
-    chkl3 = (jointval.jointsval[2]<limit.joint3b[0]);
-    chkl4 = (jointval.jointsval[3]<limit.joint4b[0]);
-    chkl5 = (jointval.jointsval[4]<limit.joint5b[0]);
-    chkl6 = (jointval.jointsval[5]<limit.joint6b[0]);
-    chku1 = (jointval.jointsval[0]>limit.joint1b[1]);
-    chku2 = (jointval.jointsval[1]>limit.joint2b[1]);
-    chku3 = (jointval.jointsval[2]>limit.joint3b[1]);
-    chku4 = (jointval.jointsval[3]>limit.joint4b[1]);
-    chku5 = (jointval.jointsval[4]>limit.joint5b[1]);
-    chku6 = (jointval.jointsval[5]>limit.joint6b[1]);
-    jointval.jointsval[0] = jointval.jointsval[0]*(!(chkl1+chku1))+(chkl1*limit.joint1b[0])+(chku1*limit.joint1b[1]);
-    jointval.jointsval[1] = jointval.jointsval[1]*(!(chkl2+chku2))+(chkl2*limit.joint2b[0])+(chku2*limit.joint2b[1]);
-    jointval.jointsval[2] = jointval.jointsval[2]*(!(chkl3+chku3))+(chkl3*limit.joint3b[0])+(chku3*limit.joint3b[1]);
-    jointval.jointsval[3] = jointval.jointsval[3]*(!(chkl4+chku4))+(chkl4*limit.joint4b[0])+(chku4*limit.joint4b[1]);
-    jointval.jointsval[4] = jointval.jointsval[4]*(!(chkl5+chku5))+(chkl5*limit.joint5b[0])+(chku5*limit.joint5b[1]);
-    jointval.jointsval[5] = jointval.jointsval[5]*(!(chkl6+chku6))+(chkl6*limit.joint6b[0])+(chku6*limit.joint6b[1]);
-
-    fobj(jointval,&tmpscore);
-    printf("thr %d tmpscore %f \n",threadIdx.x,tmpscore);
+    chkl1 = (jointshar[threadIdx.x].jointsval[0]<limit.joint1b[0]);
+    chkl2 = (jointshar[threadIdx.x].jointsval[1]<limit.joint2b[0]);
+    chkl3 = (jointshar[threadIdx.x].jointsval[2]<limit.joint3b[0]);
+    chkl4 = (jointshar[threadIdx.x].jointsval[3]<limit.joint4b[0]);
+    chkl5 = (jointshar[threadIdx.x].jointsval[4]<limit.joint5b[0]);
+    chkl6 = (jointshar[threadIdx.x].jointsval[5]<limit.joint6b[0]);
+    chku1 = (jointshar[threadIdx.x].jointsval[0]>limit.joint1b[1]);
+    chku2 = (jointshar[threadIdx.x].jointsval[1]>limit.joint2b[1]);
+    chku3 = (jointshar[threadIdx.x].jointsval[2]>limit.joint3b[1]);
+    chku4 = (jointshar[threadIdx.x].jointsval[3]>limit.joint4b[1]);
+    chku5 = (jointshar[threadIdx.x].jointsval[4]>limit.joint5b[1]);
+    chku6 = (jointshar[threadIdx.x].jointsval[5]>limit.joint6b[1]);
+    jointshar[threadIdx.x].jointsval[0] = jointshar[threadIdx.x].jointsval[0]*(!(chkl1+chku1))+(chkl1*limit.joint1b[0])+(chku1*limit.joint1b[1]);
+    jointshar[threadIdx.x].jointsval[1] = jointshar[threadIdx.x].jointsval[1]*(!(chkl2+chku2))+(chkl2*limit.joint2b[0])+(chku2*limit.joint2b[1]);
+    jointshar[threadIdx.x].jointsval[2] = jointshar[threadIdx.x].jointsval[2]*(!(chkl3+chku3))+(chkl3*limit.joint3b[0])+(chku3*limit.joint3b[1]);
+    jointshar[threadIdx.x].jointsval[3] = jointshar[threadIdx.x].jointsval[3]*(!(chkl4+chku4))+(chkl4*limit.joint4b[0])+(chku4*limit.joint4b[1]);
+    jointshar[threadIdx.x].jointsval[4] = jointshar[threadIdx.x].jointsval[4]*(!(chkl5+chku5))+(chkl5*limit.joint5b[0])+(chku5*limit.joint5b[1]);
+    jointshar[threadIdx.x].jointsval[5] = jointshar[threadIdx.x].jointsval[5]*(!(chkl6+chku6))+(chkl6*limit.joint6b[0])+(chku6*limit.joint6b[1]);
+    
+    fobj(jointshar[threadIdx.x],&tmpscore);
+//     printf("thr %d tmpscore %f \n",threadIdx.x,tmpscore);
     __syncthreads();
 
     fatomicMin(bestscore,tmpscore);
     __syncthreads();
 
     if(*bestscore==tmpscore){
-      printf("thr %d tmpscore %f \n",threadIdx.x,*bestscore);
-      *bestjoint=jointval;
+      *bestjoint=jointshar[threadIdx.x];
     }
 
     a  =  2-cyc*((2) /n_cycles);
@@ -146,28 +141,29 @@ __global__ void WaoCycle(boundaries limit,int n_cycles,float* bestscore,joints* 
   
     b=1;
     l=(a2-1)*curand_uniform(&state)+1;
-    p = curand_uniform(&state);
     
     #pragma unroll
-    for(int j=0;j<n_joints;j++){
+    for(int j=0;j<6;j++)
+    {
       if(curand_uniform(&state)<0.5)
       {
-        if fabsf(A)>=1{
-          X_rand = jointshar[floor(n_whales*curand_uniform(&state)+1)].jointsval[j];
+        if(fabsf(A)>=1)
+        {
+          X_rand = jointshar[static_cast<int>(floor((blockDim.x)*curand_uniform(&state)))].jointsval[j]; //blockDim.y e z??
           D_X_rand=abs(C*X_rand-jointshar[threadIdx.x].jointsval[j]);
           jointshar[threadIdx.x].jointsval[j]=X_rand-A*D_X_rand;
         }
         else
         {
-          jointshar[threadIdx.x].jointsval[j]=Leader_pos-A*abs(C*Leader_pos-jointshar[threadIdx.x].jointsval[j]);
+          jointshar[threadIdx.x].jointsval[j] = Leader_pos.jointsval[j]-A*abs(C*Leader_pos.jointsval[j]-jointshar[threadIdx.x].jointsval[j]);
         }
       }
       else
       {  
-        distance2Leader=abs(Leader_pos(j)-Positions(i,j));
-        Positions(i,j)=distance2Leader*exp(b.*l).*cos(l.*2*pi)+Leader_pos(j);  
+        jointshar[threadIdx.x].jointsval[j] = abs(Leader_pos.jointsval[j]-jointshar[threadIdx.x].jointsval[j])*exp(b*l)*cos(l*2*PI_F)+Leader_pos.jointsval[j];  
       }
     }
+    if(threadIdx.x==0) printf("best score : %f \n",*bestscore);
   }
 }
 
@@ -183,23 +179,28 @@ class WaoCuda
   float factor;
   float *hostBestscore  = static_cast<float *>(malloc(sizeof(float)));
   float *deviceBestscore;
+  
   joints *hostjointbest = static_cast<joints*>(malloc(sizeof(joints)));
   joints *devicejointbest;
 
   boundaries jointlimits;
+  
+  size_t shrbytes;
+  
   public:
 
-    WaoCuda(int nwhales,int ncyc,boundaries limits,int njnts);
+    WaoCuda(int nwhales,int ncyc,boundaries limits);
     void RunCycle();
 };
 
 ///////////CLASS METHODS
-WaoCuda::WaoCuda(int nwhales,int ncyc,boundaries limits,int njnts)
+WaoCuda::WaoCuda(int nwhales,int ncyc,boundaries limits)
 {
   n_whales=nwhales;
   n_cycles=ncyc;
   jointlimits=limits;
-  n_joints=njnts;
+  
+  shrbytes=n_whales*sizeof(joints);
   
   cudaMalloc(static_cast<float**>(&deviceBestscore),sizeof(float));
   memset(hostBestscore,0,sizeof(float));
@@ -210,12 +211,14 @@ WaoCuda::WaoCuda(int nwhales,int ncyc,boundaries limits,int njnts)
   cudaMemcpy(devicejointbest,hostjointbest,sizeof(joints),cudaMemcpyHostToDevice);
 //   cudaMemcpy(hostArray,deviceArray,bytes,cudaMemcpyDeviceToHost);
   
-  
+  printf("whale number: %lu \n",n_whales);
+  printf("cycles      : %lu \n",n_cycles);
+  printf("shared bytes: %lu \n",shrbytes);
 }
 
 void WaoCuda::RunCycle() //launch cuda kernel 
 {
-  WaoCycle<<<1,10>>>(jointlimits,n_cycles,deviceBestscore,devicejointbest,n_joints);//<<<blocks,thread>>>
+  WaoCycle<<<1,n_whales,shrbytes>>>(jointlimits,n_cycles,deviceBestscore,devicejointbest);//<<<blocks,thread>>>
   if (cudaSuccess != cudaDeviceSynchronize()) {
     printf("ERROR in WaoCycle\n");
     exit(-2);
@@ -239,7 +242,9 @@ int main(int argc, char *argv[]){
   limit.joint6b[0]=-360;
   limit.joint6b[1]= 360;
   
-  WaoCuda testwao(10,1,limit);
+  int ncyc=atof(argv[1]);
+  
+  WaoCuda testwao(10,ncyc,limit);//nwhales,cycles,limits,joints
   testwao.RunCycle();
   return 0;
 }
